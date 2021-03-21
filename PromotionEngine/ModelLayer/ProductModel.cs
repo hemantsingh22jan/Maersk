4ef188e4afb508c69a061d014ae055bd839ec69d
Hemant Singh Negi <38699571+hemantsingh22jan@users.noreply.github.com>
2021-03-22 00:59:09 +0530

    Create ProductModel.cs

---
+++ PromotionEngine/ModelLayer/ProductModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer
+{
+    public class ProductModel
+    {
+        public string Id { get; set; }
+        public decimal Price { get; set; }
+    }
+}
